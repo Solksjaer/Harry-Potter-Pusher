cc8ca0c93f1ab9068f7ca666ff88fbf4b8588363
Praveen Kumar Purushothaman <praveenscience@gmail.com>
2018-08-11 21:29:05 +0100

    Including the Pusher library & Instantiating.

--- routes/vote.js
+++ routes/vote.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
+const Pusher = require('pusher');
+
+const pusher = new Pusher({
+  appId: 'appId',
+  key: 'key',
+  secret: 'secret',
+  cluster: 'eu',
+  encrypted: true
+});
+
 // Default GET route.
 router.get("/", (req, res) => {
   res.send("You are in /vote.");
@@ -8,7 +18,9 @@ router.get("/", (req, res) => {
 
 // Default POST route.
 router.post("/", (req, res) => {
-  res.send("You have POSTed to /vote.");
+  pusher.trigger('my-channel', 'my-event', {
+    "message": "hello world"
+  });
 });
 
 // Export the router.
