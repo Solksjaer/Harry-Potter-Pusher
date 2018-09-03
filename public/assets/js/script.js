71436c29670f8dbd958bde734e0e1ec227a990b4
Praveen Kumar Purushothaman <praveenscience@gmail.com>
2018-09-03 20:38:55 +0100

    Added the comments for JavaScript.

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,11 +1,18 @@
+// Execute only after the whole document is fetched and assets are loaded.
 $(document).ready(function () {
+	// Form submission event listener (event handler)
 	$("#voteForm").submit(function (e) {
+		// Prevent the default event.
 		e.preventDefault();
+		// Get the checked input element's value.
 		var house = $(".form-check-input:checked").val();
+		// Construct the data to be sent as a payload to the AJAX call.
 		var data = {
 			"house": house
 		};
+		// Fire the POST request AJAX call to our /vote end point.
 		$.post("/vote", data, function (res) {
+			// Log the output in the console.
 			console.log(res);
 		});
 	});
